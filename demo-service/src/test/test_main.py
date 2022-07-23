74620dae6a3ae93bdd51bf27376b33f46e9e0e8d
slothkong <jorge.mlr.elec@gmail.com>
2022-07-23 12:24:35 -0700

    Add unit tests for demo flask app

---
+++ demo-service/src/test/test_main.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+from  main import app
+
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+
+def test_handler(client):
+    r = client.get("/")
+
+    assert r.data.decode() == "Hello, World!"
+    assert r.status_code == 200
+
